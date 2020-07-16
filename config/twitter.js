b29bbbe4120835254df9574d0722621994c1bca6
Thiago Sudré <thibook@MacBook-Air-de-Thiago.local>
2020-07-16 03:36:45 -0300

    [UPDATE] Refatoração no código

---
+++ config/twitter.js
@@ -0,0 +1,35 @@
+const Twit = require('twit')
+const twitter = new Twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
+});
+
+twitter.postaTweet = function (tweetToReply) {
+    var user = '@' + tweetToReply.in_reply_to_screen_name;
+    var tweet = user + " " + require('./ofensas-bobas.js')
+    if (user == '@thidre') {
+        tweet = user + " te amo papai!!"
+    }
+    var params = {
+        status: tweet,
+        in_reply_to_status_id: tweetToReply.in_reply_to_status_id_str,
+    }
+
+    twitter.post(
+        'statuses/update',
+        params,
+        function (err, data, response) {
+            // console.log(data);
+            if (err === undefined) {
+                console.log('tweet postado: ' + tweet);
+            }
+            else{
+                console.log(err);
+            }
+        }
+    );
+}
+
+module.exports = twitter;
