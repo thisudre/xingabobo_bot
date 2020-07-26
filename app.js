f3fc129d4a92781a9a936045889ba2fef2ceeb54
Thiago Sudré Cezarino <thiagoty@gmail.com>
2020-07-26 15:37:16 -0300

    [FIX] aspas?

--- app.js
+++ app.js
@@ -6,31 +6,7 @@ var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 //Configura o host disponível ou "0.0.0.0"
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-function buscaTweet() {
-    var stream = bot.stream('statuses/filter', {track: '@xingabobo xing'});
-    console.log("chegou aqui?");
-
-    stream.on('tweet', function(tweet){
-        console.log("tweet encontrado");
-
-        bot.curteTweet(tweet.id_str);
-
-        if (
-            (tweet.user.screen_name != 'xingabobo') 
-            && (tweet.in_reply_to_screen_name != 'xingabobo')
-            && (tweet.in_reply_to_screen_name != null)
-        )
-        {
-            bot.enviaTweet(tweet.in_reply_to_screen_name, tweet.in_reply_to_status_id_str);
-        }
-        else
-        {
-            console.log("novo tweet não postado.");
-        }
-    });
-};
-
 app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
-    buscaTweet();
+    bot.buscaTweet();
 });
